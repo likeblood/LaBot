509ba429323504876a731fd11c3b32629c6092a2
Anatoly <krest.66@mail.ru>
2020-04-10 19:09:07 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='LaBot',
+    version='0.2',
+    packages=['LaBot'],
+    url='.',
+    license='.',
+    author='Anatoly',
+    author_email='@likeblood',
+    description=''
+)
