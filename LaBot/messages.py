d655e769327901e513dddc8ed77019de8499aa40
ADmitri <admitri42@gmail.com>
2020-04-10 23:47:54 +0300

    Fixed bug and added log

--- LaBot/messages.py
+++ LaBot/messages.py
@@ -22,24 +22,28 @@ def save(bot, update):
     file_id = update.message.document.file_id
     file = bot.get_file(file_id)
     try:
-        name = update.message.document.file_name
+		name = update.message.document.file_name
+		logger.info(f"Recieved file {name} from chat {update.message.chat_id}")
         if not (pars_check.check_format(name)):
-            raise ValueError("")
+            bot.send_message(chat_id=update.message.chat_id, text=text_for_LaBot.unsaved_text, parse_mode='Markdown')
+			return None
 
         # parse number and allocate in the necessary directory
         lab_number = int(name[name.rfind('_') + 1: name.rfind('.')])
+
+
         if lab_number == 7:
-            new_path = '/Users/ba/Documents/LaBot/labs/lab_7/' + update.message.document.file_name
+            new_path = 'LaBot/labs/lab_7/' + update.message.document.file_name
         elif lab_number == 8:
-            new_path = '/Users/ba/Documents/LaBot/labs/lab_8/' + update.message.document.file_name
+            new_path = 'LaBot/labs/lab_8/' + update.message.document.file_name
         elif lab_number == 9:
-            new_path = '/Users/ba/Documents/LaBot/labs/lab_9/' + update.message.document.file_name
+            new_path = 'LaBot/labs/lab_9/' + update.message.document.file_name
         elif lab_number == 10:
-            new_path = '/Users/ba/Documents/LaBot/labs/lab_10/' + update.message.document.file_name
+            new_path = 'LaBot/labs/lab_10/' + update.message.document.file_name
         elif lab_number == 11:
-            new_path = '/Users/ba/Documents/LaBot/labs/lab_11/' + update.message.document.file_name
+            new_path = 'LaBot/labs/lab_11/' + update.message.document.file_name
         elif lab_number == 12:
-            new_path = '/Users/ba/Documents/LaBot/labs/lab_12/' + update.message.document.file_name
+            new_path = 'LaBot/labs/lab_12/' + update.message.document.file_name
         else:
             bot.send_message(chat_id=update.message.chat_id, text=text_for_LaBot.unsaved_text, parse_mode='Markdown')
         file.download(new_path)
@@ -47,5 +51,5 @@ def save(bot, update):
                          parse_mode='Markdown')
 
     except Exception:
-        bot.send_message(chat_id=update.message.chat_id, text=text_for_LaBot.unsaved_text, parse_mode='Markdown')
+		bot.send_message(chat_id=update.message.chat_id, text=text_for_LaBot.sth_wrong, parse_mode='Markdown')
         logger.error('Something wrong', exc_info=True)
